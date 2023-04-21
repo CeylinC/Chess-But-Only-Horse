879e625f16ce2996d6894b0e3efe90f40282d10c
Ceylin <caltepeceylin@gmail.com>
2023-04-21 12:57:44 +0300

    feat: create board

--- ChessButOnlyHorse/ChessButOnlyHorse/Form1.cs
+++ ChessButOnlyHorse/ChessButOnlyHorse/Form1.cs
@@ -16,5 +16,36 @@ namespace ChessButOnlyHorse
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] board = new Button[10, 10];
+            CreateBoard(board);
+        }
+
+        private void CreateBoard(Button[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i,j] = CreateButton(i,j);
+                }
+            }
+        }
+
+        private Button CreateButton(int satir, int sutun)
+        {
+            Button button = new Button();
+            button.Size = new Size(30, 30);
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0;
+            button.FlatAppearance.MouseOverBackColor = Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            button.Margin = new Padding(0, 0, 0, 0);
+            button.BackColor = (satir % 2 == 0) ? (sutun%2==0 ? Color.WhiteSmoke: Color.White) : (sutun%2==0 ? Color.White: Color.WhiteSmoke);
+            panel_board.Controls.Add(button);
+            return button;
+            
+        }
     }
 }
