1d43ec42f0c2ab8c0b8d0ee5308a75f1318f3b69
Ceylin <caltepeceylin@gmail.com>
2023-04-21 23:48:51 +0300

    feat: add buttons, label and winner and gameover screen

--- ChessButOnlyHorse/ChessButOnlyHorse/Form1.cs
+++ ChessButOnlyHorse/ChessButOnlyHorse/Form1.cs
@@ -15,6 +15,7 @@ namespace ChessButOnlyHorse
     {
         Button[,] board;
         bool firstClick;
+        int score = 0, size = 10;
         public Form1()
         {
             InitializeComponent();
@@ -24,9 +25,9 @@ namespace ChessButOnlyHorse
         {
             board = new Button[10, 10];
             firstClick = true;
+            score= 0;
             CreateBoard(); 
         }
-
         private void CreateBoard()
         {
             for (int i = 0; i < board.GetLength(0); i++)
@@ -39,7 +40,6 @@ namespace ChessButOnlyHorse
             }
             ColorBoard();
         }
-
         private Button CreateButton()
         {
             Button button = new Button();
@@ -59,14 +59,12 @@ namespace ChessButOnlyHorse
             panel_board.Controls.Add(button);
             
             return button;
-            
         }
-
         private void ColorBoard()
         {
-            for (int i = 0; i < board.GetLength(0); i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < board.GetLength(1); j++)
+                for (int j = 0; j < size; j++)
                 {
                     board[i,j].BackColor = (i % 2 == 0) ? (j % 2 == 0 ? Color.WhiteSmoke : Color.White) : (j % 2 == 0 ? Color.White : Color.WhiteSmoke);
                     if ((bool)board[i,j].Tag)
@@ -80,6 +78,43 @@ namespace ChessButOnlyHorse
                 }
             }
         }
+        private void ClearBoard()
+        {
+            panel_board.Controls.Clear();
+            switch (size)
+            {
+                case 5:
+                    panel_board.Size = new Size(150, 150);
+                    break;
+                case 6:
+                    panel_board.Size = new Size(200, 200);
+                    break;
+                case 7:
+                    panel_board.Size = new Size(220, 220);
+                    break;
+                case 8:
+                    panel_board.Size = new Size(255, 255);
+                    break;
+                case 9:
+                    panel_board.Size = new Size(280, 280);
+                    break;
+                case 10:
+                    panel_board.Size = new Size(300, 300);
+                    break;
+                default:
+                    break;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j].Tag = false;
+                    board[i, j].Enabled = true;
+                    panel_board.Controls.Add(board[i,j]);
+
+                }
+            }
+        }
         private void ClickButton(object sender, EventArgs e)
         {
             ColorBoard();
@@ -88,13 +123,14 @@ namespace ChessButOnlyHorse
             {
                 firstClick = false;
                 clickedButton.Tag = true;
+                label_score.Text = $"Score: {++score}";
                 DisplayMove(clickedButton);
 
             }
         }
-
-        private void ButtonControl(Dictionary<char, int> location, int i, int j)
+        private bool ButtonControl(Dictionary<char, int> location, int i, int j)
         {
+            bool a = false, b = false, c = false, d = false; //Hareket etme hakkı sıfırlanırsa kaybetmiş olacak
             if (location['y'] - i >= 0)
             {
                 if (location['x'] - j >= 0)
@@ -103,19 +139,21 @@ namespace ChessButOnlyHorse
                     {
                         board[location['y'] - i, location['x'] - j].BackColor = Color.MediumPurple;
                         board[location['y'] - i, location['x'] - j].Enabled= true;
+                        a = true;
                     }
                 }
-                if (location['x'] + j < board.GetLength(1))
+                if (location['x'] + j < size)
                 {
                     if (!(bool)board[location['y'] - i, location['x'] + j].Tag)
                     {
                         board[location['y'] - i, location['x'] + j].BackColor = Color.MediumPurple;
                         board[location['y'] - i, location['x'] + j].Enabled = true;
+                        b = true;
                     }
                 }
             }
 
-            if (location['y'] + i < board.GetLength(0))
+            if (location['y'] + i < size)
             {
                 if (location['x'] - j >= 0)
                 {
@@ -123,27 +161,38 @@ namespace ChessButOnlyHorse
                     {
                         board[location['y'] + i, location['x'] - j].BackColor = Color.MediumPurple;
                         board[location['y'] + i, location['x'] - j].Enabled = true;
+                        c = true;
                     }
                 }
-                if (location['x'] + j < board.GetLength(1))
+                if (location['x'] + j < size)
                 {
                     if (!(bool)board[location['y'] + i, location['x'] + j].Tag)
                     {
                         board[location['y'] + i, location['x'] + j].BackColor = Color.MediumPurple;
                         board[location['y'] + i, location['x'] + j].Enabled = true;
+                        d = true;
                     }
                 }
             }
+            return a||b||c||d;
         }
-
         private void DisplayMove(Button clickedButton)
         {
-            Dictionary<char, int> location = FindButton(clickedButton);
             ColorBoard();
-            ButtonControl(location, 1, 2);
-            ButtonControl(location, 2, 1);
+            Dictionary<char, int> location = FindButton(clickedButton);
+            if (!(ButtonControl(location, 2, 1) | ButtonControl(location, 1, 2)))
+            {
+                if (FinishGameControl())
+                {
+                    MessageBox.Show("!!!Winner!!!");
+                }
+                else
+                {
+                    DisplayGameOver();
+                }
+            }
+            
         }
-
         private Dictionary<char, int> FindButton(Button clickedButton)
         {
             Dictionary<char, int> button = new Dictionary<char, int>();
@@ -162,6 +211,36 @@ namespace ChessButOnlyHorse
                 }
             }
             return null;
+        }
+        private void button_play_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            firstClick = true;
+            ClearBoard();
+            ColorBoard();
+        }
+        private void button_size_Click(object sender, EventArgs e)
+        {
+            size = (size == 10) ? 5 : ++size;
+            button_size.Text = $"{size}x{size}";
+        }
+        private void DisplayGameOver()
+        {
+            MessageBox.Show($"Game Over\nScore: {score}");
+        }
+        private bool FinishGameControl()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (!(bool)board[i, j].Tag)
+                    {
+                        return false; //oyun bitmedi
+                    }
+                }
+            }
+            return true; //oyun bitti
         }
     }
 }
